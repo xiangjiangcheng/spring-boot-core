c62a3944c649fee6f698d0e9b71cdbb344b39cd8
xjc <1150241658@qq.com>
2018-05-23 18:23:57 +0800

    添加jackSon,json转换工具类

---
+++ src/main/java/utils/JsonUtil.java
@@ -0,0 +1,35 @@
+package utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+/**
+ * @author xiangjiangcheng
+ * @Description json转换工具类
+ * @modifyTime 2018/5/23 18:21
+ */
+public class JsonUtil {
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    public static String convertObj2String(Object object) {
+        String s = null;
+        try {
+            s = objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return s;
+    }
+
+    public static <T> T convertString2Obj(String s, Class<T> clazz) {
+        T t = null;
+        try {
+            t = objectMapper.readValue(s, clazz);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return t;
+    }
+}
