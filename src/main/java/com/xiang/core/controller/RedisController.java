abd3f407fbcc2ca83f5610858b0a2227311dc342
xjc <1150241658@qq.com>
2018-05-23 18:31:14 +0800

    redisController说明

---
+++ src/main/java/com/xiang/core/controller/RedisController.java
@@ -0,0 +1,16 @@
+package com.xiang.core.controller;
+
+/**
+ * @author : xiangjiangcheng
+ * github: https://github.com/xiangjiangcheng
+ * email: jiangcheng.xiang@qq.com
+ * <p>
+ * Date: 2018/05/23 18:00
+ * Description: 使用自动配置的StringRedisTemplate对象进行Redis读写操作
+ * Copyright(©) 2018 by xiangjiangcheng.
+ * 参考url https://www.cnblogs.com/guanzhyan/p/8367585.html
+ */
+public class RedisController {
+
+
+}
